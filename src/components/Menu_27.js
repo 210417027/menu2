6ae51c2f6de2c3c29fd523dea00469a06b911886
210417027 <210417027@gms.thu.edu.tw>
2022-12-18 17:36:52 +0800

    1

--- src/components/Menu_27.js
+++ src/components/Menu_27.js
@@ -11,12 +11,12 @@ const Menu_27 = ( {items}) => {
             src={img}
             alt="buttermilk"
             pancakes=""
-            class="photo"
+            className="photo"
           />
-          <div class="item-info">
+          <div className="item-info">
             <header>
               <h4>{title}</h4>
-              <h4 class="price">${price}</h4>
+              <h4 className="price">${price}</h4>
             </header>
             <p className="item-text">
               {desc}
